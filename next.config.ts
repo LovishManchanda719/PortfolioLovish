29fd245e558cc2eebeae46bb3125bce56e170a49
LovishManchanda719 <154732716+LovishManchanda719@users.noreply.github.com>
2025-01-30 16:26:01 +0530

    Save local changes before pulling

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
-    domains: ['drive.google.com', 'lh3.googleusercontent.com'], // Add lh3.googleusercontent.com
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com', // Add this pattern
-        pathname: '**',
-      },
-    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
