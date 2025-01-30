4aad2a2ec17bf0eff4d9c0c318455b81be14d74e
LovishManchanda719 <154732716+LovishManchanda719@users.noreply.github.com>
2025-01-30 18:32:33 +0530

    Changes 4

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,8 @@
 "use client";
-import AuthPage from '@/components/auth/AuthPage';
+import dynamic from 'next/dynamic';
+
+// Dynamically import the AuthPage component and disable SSR
+const AuthPage = dynamic(() => import('@/components/auth/AuthPage'), { ssr: false });
 
 export default function Authentication() {
   return <AuthPage />;
