001182572e80cee5fce1fc30641f5d3a341715a0
LovishManchanda719 <154732716+LovishManchanda719@users.noreply.github.com>
2025-01-30 17:09:42 +0530

    commit attempt1

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,23 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  basePath: '/Portfolio', // Replace 'Portfolio' with your repository name
+  assetPrefix: '/Portfolio/', // Replace 'Portfolio' with your repository name
   images: {
     unoptimized: true,
+    domains: ['drive.google.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
